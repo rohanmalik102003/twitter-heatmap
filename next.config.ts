e091264cbc32e1f968aada4316630fcfb9a1c362
rohanmalik102003 <rohanmalik102003@gmail.com>
2025-09-08 13:17:35 +0530

    small changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
   experimental: {
     optimizePackageImports: ['lucide-react']
-  },
-  images: {
-    unoptimized: true
   }
 };
 
